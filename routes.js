06b266199c89aeaf09bf698c5fc6db9c4269348c
Qupi <vorteg.r@gmail.com>
2020-10-14 17:54:34 -0500

    First commit

---
+++ routes.js
@@ -0,0 +1,14 @@
+const routes = [
+    {
+        path: '/',
+        template: '<h1>Hola</h1>'
+    },
+    {
+        path: '/contacto',
+        template: '<h1>Contacto mi correo r@micorreo.com</h1>'        
+    },
+    {
+        path: '/about',
+        template: '<h1>About Me</h1>'   
+    },
+];
